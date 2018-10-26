d706ca880e83bc52ac382027b34ba21acc90218b
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-26 18:19:11 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,7 @@ def square_array(array)
   # your code here
   newArray = []
   array.each do |number|
-    return newArray.push(number**2)
+    newArray.push(number**2)
   end
+  return newArray
 end
